fa1417cfe23592668f008e001aa51b51a30ae666
ramonabayssari <102956605+ramonabayssari@users.noreply.github.com>
2022-12-17 16:08:27 +0200

    back end- set up

---
+++ back_end/reach_out/app/Models/Applies_for.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Applies_for extends Model
+{
+    use HasFactory;
+}
