c34cea3ec1db17bd0873187257723088d35a3ac4
ramonabayssari <102956605+ramonabayssari@users.noreply.github.com>
2022-12-19 23:18:42 +0200

    back end- fixing table

--- back_end/reach_out/database/migrations/2022_12_17_135420_create_volunteers.php
+++ back_end/reach_out/database/migrations/2022_12_17_135420_create_volunteers.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('volunteers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->int("user_id");
+            $table->integer("user_id");
             $table->string("occupatation");
             $table->string("resume");
             $table->string("profile_pic");
