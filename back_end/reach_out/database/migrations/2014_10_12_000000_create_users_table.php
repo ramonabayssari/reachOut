9c2ffcfed396c19e863c4a852328637a558c9082
ramonabayssari <102956605+ramonabayssari@users.noreply.github.com>
2022-12-19 23:19:00 +0200

    back end- fixing table

--- back_end/reach_out/database/migrations/2014_10_12_000000_create_users_table.php
+++ back_end/reach_out/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->int("is_volunteer");
         });
     }
 
