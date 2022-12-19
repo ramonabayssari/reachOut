546f50b41b010633c08b8554ee539d191bdf4038
ramonabayssari <102956605+ramonabayssari@users.noreply.github.com>
2022-12-19 13:38:56 +0200

    front end- adding js functions

--- front_end/functions.js
+++ front_end/functions.js
@@ -1,61 +1,91 @@
-window.onload = function () {
-    const hands= document.getElementById("hands");
-    const email= document.getElementById("email");
-    const uname= document.getElementById("uname");
-    const fullname=document.getElementById("fullname");
-    const psw=document.getElementById("psw");
-    const about=document.getElementById("about");
-    const occ=document.getElementById("occ");
-    const unameL= document.getElementById("unameL");
+    const base_url="Http://localhost:8000/api/"
+
+    const pages = {}
+
+    pages.postAPI = async(api_url, data, api_token = null) => {  
+        try {
+            return await axios.post(
+                api_url, 
+                data,
+                {
+                    headers:{
+                        'Authorization': `Bearer ${api_token}`
+                    }
+                }
+            )
+        } catch (error) {
+            console.log("Error from linking (POST)", error)
+        }
+        
+    }
+    
+    pages.loadFor = (page) => { 
+        eval("pages.load_" + page + "();")
+      }
+    
+      
+      const hands= document.getElementById("hands");
+      let email_= document.getElementById("email");
+      const fullname=document.getElementById("fullname");
+      const psw=document.getElementById("psw");
+      const about=document.getElementById("about");
+      const occ=document.getElementById("occ");
+
+      pages.load_signup = () =>{
+        const api_url = base_url + "/auth/login"
+
+        email_.addEventListener("mouseover", function(){
+            hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
+        })
+        email_.addEventListener("mouseout", function(){
+            hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
+        })
+    
+       
+    
+        fullname.addEventListener("mouseover", function(){
+            hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
+        })
+        fullname.addEventListener("mouseout", function(){
+            hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
+        })
+    
+        psw.addEventListener("mouseover", function(){
+            hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
+        })
+        psw.addEventListener("mouseout", function(){
+            hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
+        })
+    
+        about.addEventListener("mouseover", function(){
+            hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
+        })
+        about.addEventListener("mouseout", function(){
+            hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
+        })
+    
+        occ.addEventListener("mouseover", function(){
+            hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
+        })
+        occ.addEventListener("mouseout", function(){
+            hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
+        })        
+    }
+
+    
+
+ //-----------------------------------------------------------------------------------------------
+    const emailL= document.getElementById("emailL");
     const pswL=document.getElementById("pswL");
     const handsL= document.getElementById("handsL");
+    let subLogin = document.getElementById("login_sub")
 
-    email.addEventListener("mouseover", function(){
-        hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    email.addEventListener("mouseout", function(){
-        hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
+    pages.load_login = () =>{
 
-    uname.addEventListener("mouseover", function(){
-        hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    uname.addEventListener("mouseout", function(){
-        hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
-
-    fullname.addEventListener("mouseover", function(){
-        hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    fullname.addEventListener("mouseout", function(){
-        hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
-
-    psw.addEventListener("mouseover", function(){
-        hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    psw.addEventListener("mouseout", function(){
-        hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
-
-    about.addEventListener("mouseover", function(){
-        hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    about.addEventListener("mouseout", function(){
-        hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
-
-    occ.addEventListener("mouseover", function(){
-        hands.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    occ.addEventListener("mouseout", function(){
-        hands.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
-
-    unameL.addEventListener("mouseover", function(){
+    emailL.addEventListener("mouseover", function(){
         handsL.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
     })
-    unameL.addEventListener("mouseout", function(){
+    emailL.addEventListener("mouseout", function(){
         handsL.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
     })
 
@@ -65,25 +95,90 @@ window.onload = function () {
     pswL.addEventListener("mouseout", function(){
         handsL.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
     })
-}
 
-window.onload = function () {
- 
-    const unameL= document.getElementById("unameL");
-    const pswL=document.getElementById("pswL");
-    const handsL= document.getElementById("handsL");
+    subLogin.addEventListener("click", async function(){
+        const signin_url= base_url+"auth/login"
+        const auth = new FormData()
+        const e= emailL.value
+        const p= pswL.value
+    
+        auth.append("email",e)
+        auth.append("password", p)
+    
+        const response= await pages.postAPI(signin_url, auth)
+        alert(response)
+        console.log(response)
+    
+        if(response){
+            alert('if')
+            console.log(response)
+            
+          }
+          else{
+            console.log("inco")
+            alert('else')
+          }
+    
+    })
+    }
 
-    unameL.addEventListener("mouseover", function(){
-        handsL.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    unameL.addEventListener("mouseout", function(){
-        handsL.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
+    //-----------------------------------------------------------------------------------------
+    pages.load_homepage= () =>{
+        const categoryButton = document.querySelector('.category-button');
+        const categoryBox = document.querySelector('.category-box');
+        const profile=document.getElementById("profile")
 
-    pswL.addEventListener("mouseover", function(){
-        handsL.innerHTML='<img src="assets/signup.gif" class="gif-size center-gif">';
-    })
-    pswL.addEventListener("mouseout", function(){
-        handsL.innerHTML='<img src="assets/signup.png" class="gif-size center-gif">';
-    })
-}
+        categoryButton.addEventListener('click', function() {
+        if (categoryBox.style.display === "none") {
+            categoryBox.style.display = "block";
+        } else {
+            categoryBox.style.display = "none";
+        }
+        });
+        
+        profile.addEventListener("click",function(){
+            window.location.replace("volunteer_profilepage.html");
+        })
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    pages.load_profilepage=()=>{
+        const btn1= document.getElementById("btn1")
+        const home=document.getElementById("home")
+
+        function printPDF() {
+          const divToRemove = document.getElementById("side-bar");
+          divToRemove.style.display = "none";
+          window.print();
+          divToRemove.style.display = "block";
+        }
+        btn1.addEventListener("click", printPDF); 
+
+        home.addEventListener("click", function(){
+            window.location.replace("volunteer_homepage.html");
+        })
+    }
+    //------------------------------------------------------------------------------------------------------
+
+    pages.load_Chomepage= () =>{
+        const profile=document.getElementById("profile")
+        
+        profile.addEventListener("click",function(){
+            window.location.replace("company_profilepage.html");
+        })
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+
+    pages.load_Cprofilepage=()=>{
+        const home= document.getElementById("home")
+        const btn1= document.getElementById("btn1")
+
+        home.addEventListener("click", function(){
+            window.location.replace("company_homepage.html");
+        })
+
+        bnt1.addEventListener("click",function(){
+            window.location.replace("newevent.html");
+        })
+    }
